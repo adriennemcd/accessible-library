59593b9ecb2fcbca7b98e28cd98c655ed0d8a218
adriennemcd <adrienne.mcd.21@gmail.com>
2018-01-16 17:15:04 -0500

    continue to work on validation js, add some error styles

--- _includes/components/form/form.js
+++ _includes/components/form/form.js
@@ -5,6 +5,7 @@ function form(formObj, i){
       $indx = i,
       $txtInputs = $form.querySelectorAll('.form__text'),
       $messages = $form.querySelector('.error__list'),
+      $message = $form.querySelectorAll('.error__item'),
       $screenReaderErrors = $form.querySelector('.error__link'),
       $formValidation = {};
 
@@ -36,17 +37,45 @@ function form(formObj, i){
     $form.addEventListener('submit', function(e) {
       e.preventDefault();
       // Each time the user tries to send the data, we check if the form is valid.
-      if ( checkFormValidity(this) ) {
+      if ( checkFormValidity() ) {
         console.log('valid');
-        //screenReaderErrors.removeClass("active");
-        //messages.removeClass("active");
+        $screenReaderErrors.classList.remove("active");
+        $messages.classList.remove("active");
         //success.fadeIn("fast");
         //success.on("click", function(){ $(this).hide(); });
       } else {
         console.log('not valid');
-        //handleFormErrors();
+        handleFormErrors();
       }
     });
+
+    $screenReaderErrors.addEventListener('click', function(e){
+      e.preventDefault();
+      $messages.classList.add('active');
+      $messages.querySelector('.error__item.active').focus();
+    });
+
+    $message.forEach(function(error) {
+      error.addEventListener("click", function(e){
+        e.preventDefault();
+        var input = e.currentTarget.dataset.input;
+        $form.querySelector("[name='"+input+"']").focus();
+      });
+    })
+  }
+
+  function handleFormErrors() {
+    var count = 0;
+    for ( var i in $formValidation) {
+      if ( $formValidation[i] === false ) {
+        count++;
+        var inputEl = $form.querySelector('[name="'+i+'"]');
+        addError(inputEl);
+      }
+    }
+    $screenReaderErrors.innerHTML = ("There are "+count+" errors in the form you submitted. Review Errors.");
+    $screenReaderErrors.classList.add('active');
+    $screenReaderErrors.focus();
   }
 
   // Check validity
@@ -56,24 +85,24 @@ function form(formObj, i){
     return valid;
   }
 
-  function checkFormValidity(form){
+  function checkFormValidity(){
     var formValid;
     
     // add input state to formValidation object
-    // $(form).find("input[type='text']").each(function(i){
-    //   checkInputValidity(this) ? formValidation[this.name] = true : formValidation[this.name] = false;
-    // });
+    $txtInputs.forEach(function(input){
+      checkInputValidity(input) ? $formValidation[input.name] = true : $formValidation[input.name] = false;
+    });
 
-    // //check if any inputs are invalid
-    // for ( var i in formValidation) {
-    //   if ( formValidation[i] === false ) {
-    //     formValid = false;
-    //     break;
-    //   } else {
-    //     formValid = true;
-    //   }
-    // }
-    // return formValid;
+    //check if any inputs are invalid
+    for ( var i in $formValidation) {
+      if ( $formValidation[i] === false ) {
+        formValid = false;
+        break;
+      } else {
+        formValid = true;
+      }
+    }
+    return formValid;
   }
 
   // Handle errors in the DOM

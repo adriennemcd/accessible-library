3ec00a2d034f54e0dee0e6176bab9ccc35058626
adriennemcd <adrienne.mcd.21@gmail.com>
2018-01-22 16:27:42 -0500

    update event listeners for all form input types

--- _includes/components/form/form.js
+++ _includes/components/form/form.js
@@ -5,36 +5,56 @@ function form(formObj){
       $txtInputs = $form.querySelectorAll('.form__text'),
       $collectionInputs = $form.querySelectorAll('.form__fieldset'),
       $selectInputs = $form.querySelectorAll('.form__select'),
+      $screenReaderErrors = $form.querySelector('.error__link'),
       $errorList = $form.querySelector('.error__list'),
       $errorItem = $form.querySelectorAll('.error__item'),
-      $screenReaderErrors = $form.querySelector('.error__link'),
       $formValidation = {};
 
   function init() {
     handleEvents();
   }
 
+  // INITIALIZE ALL EVENT LISTENERS ====================================
+
   function handleEvents() {
+    function checkItemValidity(){ !checkInputValidity(this) ? addError(this) : removeError(this); }
+
     $txtInputs.forEach(function(input) {
+      // check validity when typing in text inputs
       input.addEventListener('keyup', function(e){
-        checkInputValidity(input);
-        if (checkInputValidity(input)) {
-          removeError(input);
-          addSuccess(input);
+        checkInputValidity(this);
+        if (checkInputValidity(this)) {
+          removeError(this);
+          addSuccess(this);
         } else {
-          removeSuccess(input);
+          removeSuccess(this);
         }
       });
 
-      input.addEventListener('blur', function(e) {
-        if (!checkInputValidity(input)) {
-          addError(input);
-        } else {
-          removeError(input);
-        }
+      // check validity when focus leaves text inputs
+      input.addEventListener('blur', checkItemValidity);
+    });
+
+    // check validity when focus leaves radio buttons and checkboxes
+    $collectionInputs.forEach(function(group){
+      var groupItems = group.querySelectorAll('.form__input');
+      groupItems.forEach(function(input){
+        input.addEventListener('click', function(){
+          !checkInputValidity(group) ? addError(this) : removeError(this);
+        });
+        input.addEventListener('blur', function(){
+          !checkInputValidity(group) ? addError(this) : removeError(this);
+        });
       });
     });
 
+    // check validity when focus leaves select
+    $selectInputs.forEach(function(select){
+      select.addEventListener('change', checkItemValidity);
+      select.addEventListener('blur', checkItemValidity);
+    });
+
+    // check validity when form is submitted
     $form.addEventListener('submit', function(e) {
       e.preventDefault();
       // Each time the user tries to send the data, we check if the form is valid.
@@ -50,12 +70,14 @@ function form(formObj){
       }
     });
 
+    // listen for when hidden screen reader error link is clicked
     $screenReaderErrors.addEventListener('click', function(e){
       e.preventDefault();
       $errorList.classList.add('active');
       $errorList.querySelector('.error__item.active').focus();
     });
 
+    // listen for when hidden screen reader error descriptions are clicked
     $errorItem.forEach(function(error) {
       error.addEventListener("click", function(e){
         e.preventDefault();
@@ -65,7 +87,8 @@ function form(formObj){
     })
   }
 
-  // Check validity
+  // CHECK INPUT AND FORM VALIDITY =====================================
+
   function checkInputValidity(input){
     var valid,
         groupItems;
@@ -76,13 +99,13 @@ function form(formObj){
       groupItems.forEach(function(item) {
         item.checked ? groupItemCount++ : groupItemCount;
       });
-      groupItemCount > 0 ? valid = true : valid = false;
+      valid = groupItemCount > 0 ? true : false;
       $formValidation[input.dataset.name] = valid;
       return valid;
     } 
     else if (input['type'].indexOf('select') >= 0) {
       // select-one or select-many form items
-      input.selectedIndex !== 0 ? valid = true : valid = false;
+      valid = input.selectedIndex !== 0 ? true : false;
       $formValidation[input.name] = valid;
       return valid;
     } 
@@ -99,15 +122,15 @@ function form(formObj){
     
     // add input state to formValidation object
     $txtInputs.forEach(function(input){
-      checkInputValidity(input) ? $formValidation[input.name] = true : $formValidation[input.name] = false;
+      $formValidation[input.name] = checkInputValidity(input) ? true : false;
     });
 
     $collectionInputs.forEach(function(input){
-      checkInputValidity(input) ? $formValidation[input.dataset.name] = true : $formValidation[input.dataset.name] = false;
+      $formValidation[input.dataset.name] = checkInputValidity(input) ? true : false;
     });
 
     $selectInputs.forEach(function(input){
-      checkInputValidity(input) ? $formValidation[input.name] = true : $formValidation[input.name] = false;
+      $formValidation[input.name] = checkInputValidity(input) ? true : false;
     });
 
     //check if any inputs are invalid
@@ -121,6 +144,8 @@ function form(formObj){
     }
     return formValid;
   }
+
+  // HANDLE ERRORS =====================================================
 
   function handleFormErrors() {
     console.log($formValidation);
@@ -139,30 +164,38 @@ function form(formObj){
 
   // Handle errors in the DOM
   function addError(input){
-    var inputErrorMsg = $form.querySelector('a[data-input="'+input.name+'"]');
-    inputErrorMsg.classList.add('active');
-    input.parentElement.classList.add('error');
+    console.log(input);
+    var srErrorMsg = $form.querySelector('a[data-input="'+input.name+'"]');
+    var capitalInput = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+    var wrapper = input.closest('.form__item'); // NEED .CLOSEST POLYFILL FOR IE10, IE11
+    var inputErrorMsg = wrapper.querySelector('.form__error-message');
+
+    srErrorMsg.classList.add('active');
+    wrapper.classList.add('error');
     
     if (input.validity.valueMissing) {
-      input.nextElementSibling.innerHTML = input.name+' is required';
-      inputErrorMsg.innerHTML = input.name+' is required';
+      inputErrorMsg.innerHTML = capitalInput+' is required';
+      srErrorMsg.innerHTML = capitalInput+' is required';
     } else {
-      input.nextElementSibling.innerHTML = input.name+' format is invalid';
-      inputErrorMsg.innerHTML = input.name+' format is invalid';
+      inputErrorMsg.innerHTML = capitalInput+' format is invalid';
+      srErrorMsg.innerHTML = capitalInput+' format is invalid';
     }
   }
 
   function removeError(input){
-    input.parentElement.classList.remove('error');
+    var wrapper = input.closest('.form__item');
+    wrapper.classList.remove('error');
     $form.querySelector('a[data-input="'+input.name+'"]').classList.remove('active');
   }
 
   function addSuccess(input) {
-    input.parentElement.classList.add('success');
+    var wrapper = input.closest('.form__item');
+    wrapper.classList.add('success');
   }
 
   function removeSuccess(input) {
-    input.parentElement.classList.remove('success');
+    var wrapper = input.closest('.form__item');
+    wrapper.classList.remove('success');
   }
 
   return {
@@ -171,7 +204,7 @@ function form(formObj){
 };
 
 //call the form module for each .form on the page
-var forms = document.querySelectorAll('.form');
+var forms = document.querySelectorAll('.js-form');
 forms.forEach(function(formObj){
   var newForm = form(formObj);
   newForm.init();
